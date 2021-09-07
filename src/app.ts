c4cfe1e6ba4013ca0d834c3969d6dd75cf8f588d
acrylic-style <me@acrylicstyle.xyz>
2021-09-08 03:31:01 +0900

    Let's configure CORS properly

--- src/app.ts
+++ src/app.ts
@@ -55,7 +55,7 @@ app.use(logger('dev', {
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({ origin: process.env.APP_URL }))
 //app.use(express.static('./public'))
 
 /*
